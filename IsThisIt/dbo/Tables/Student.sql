639bda6826438ec41defef33a05f32cc8de538b3
Domingo Gomes <domingoashishgomes@outlook.com>
2020-06-29 12:06:25 +0530

    All objects

---
+++ IsThisIt/dbo/Tables/Student.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Student] (
+    [Id]    NCHAR (10) NULL,
+    [Name]  NCHAR (10) NULL,
+    [Class] INT        NULL, 
+    [Address] NVARCHAR(50) NULL
+);
+
---
+++ IsThisIt/dbo/Tables/Teacher.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Teacher] (
+    [Id]     NCHAR (10) NULL,
+    [Name]   NCHAR (10) NULL,
+    [School] NCHAR (10) NULL,
+    [Class]  NCHAR (10) NULL
+);
+
