b661ed21db136d8891063549e9a120ba6229cde9
Domingo Gomes <domingoashishgomes@outlook.com>
2020-07-03 14:20:54 +0530

    Changes

--- IsThisIt/dbo/Tables/Class.sql
+++ IsThisIt/dbo/Tables/Class.sql
@@ -3,5 +3,6 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Name] NVARCHAR(50) NULL, 
     [Capacity] INT NULL, 
-    [Sections] NCHAR(10) NULL
+    [Sections] NCHAR(10) NULL, 
+    [Teacher] NCHAR(10) NULL
 )
